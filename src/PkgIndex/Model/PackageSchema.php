82c2d3861a4b34c95ba5b9b5f3f917d17813edeb
c9s <yoanlin93@gmail.com>
2016-06-19 15:47:37 +0800

    update package schema

--- src/PkgIndex/Model/PackageSchema.php
+++ src/PkgIndex/Model/PackageSchema.php
@@ -6,15 +6,26 @@ class PackageSchema extends DeclareSchema
 {
     public function schema()
     {
-        $this->column('vendor_name')
+        $this->column('vendor')
             ->varchar(30)
             ;
-        $this->column('package_name')
+
+        $this->column('name')
             ->varchar(30)
             ;
-        $this->column('package_type')
+
+        $this->column('package_system')
             ->varchar(30)
             ;
+
+        $this->column('package_system_source')
+            ->varchar(60)
+            ;
+
+        $this->column('url')
+            ->varchar(255)
+            ;
+
         $this->hasMany('releases', 'PkgIndex\Model\ReleaseSchema', 'package_id', 'id');
     }
 }
